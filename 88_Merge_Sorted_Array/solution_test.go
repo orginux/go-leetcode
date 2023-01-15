7425eb9ce09920658570688dde05441a08105951
dgorobets <Denis.Gorobets@infobip.com>
2023-01-15 13:07:43 +0100

    minot fix tests

--- 88_Merge_Sorted_Array/solution_test.go
+++ 88_Merge_Sorted_Array/solution_test.go
@@ -11,10 +11,10 @@ func TestMerge_1(t *testing.T) {
 		nums1, m := []int{1, 2, 3, 0, 0, 0}, 3
 		nums2, n := []int{2, 5, 6}, 3
 
-		answer := []int{1, 2, 2, 3, 5, 6}
-		result := merge(nums1, m, nums2, n)
+		want := []int{1, 2, 2, 3, 5, 6}
+		got := merge(nums1, m, nums2, n)
 
-		assert.Equal(t, result, answer)
+		assert.Equal(t, got, want)
 	})
 
 	t.Run("Case 2", func(t *testing.T) {
@@ -22,10 +22,10 @@ func TestMerge_1(t *testing.T) {
 		nums1, m := []int{1}, 1
 		nums2, n := []int{}, 0
 
-		answer := []int{1}
-		result := merge(nums1, m, nums2, n)
+		want := []int{1}
+		got := merge(nums1, m, nums2, n)
 
-		assert.Equal(t, result, answer)
+		assert.Equal(t, got, want)
 
 	})
 	t.Run("Case 3", func(t *testing.T) {
@@ -33,10 +33,10 @@ func TestMerge_1(t *testing.T) {
 		nums1, m := []int{0}, 0
 		nums2, n := []int{1}, 1
 
-		answer := []int{1}
-		result := merge(nums1, m, nums2, n)
+		want := []int{1}
+		got := merge(nums1, m, nums2, n)
 
-		assert.Equal(t, result, answer)
+		assert.Equal(t, got, want)
 
 	})
 }
