9e879d115081ce050c60ca14691a62ea9c62b339
dgorobets <Denis.Gorobets@infobip.com>
2023-01-17 20:37:57 +0100

    added 118

---
+++ 118_Pascals_Triangle/solution.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	numRows := 5
+	fmt.Println(generate(numRows))
+}
+
+func generate(numRows int) [][]int {
+	result := make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		result[i] = make([]int, i+1)
+		result[i][0] = 1
+		result[i][i] = 1
+
+		for j := 1; j < i; j++ {
+			result[i][j] = result[i-1][j-1] + result[i-1][j]
+		}
+	}
+
+	return result
+}
